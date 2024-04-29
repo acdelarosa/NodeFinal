46643dede9784118b847dee5bf4837615ce35458
acdelarosa <andlr31@hotmail.com>
2024-04-29 00:12:12 -0500

    Modelo Users listo

--- Models/Users.js
+++ Models/Users.js
@@ -1,2 +1,20 @@
 const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema;
+const userSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        require: true
+    },
+    edad:{
+        type: Number,
+        require: true
+    },
+    email:{
+        type: String,
+        require: true,
+        unique: true
+    }
+})
+
+const User = mongoose.model("User", mongoose.userSchema);
+
+module.exports= User;
