f835dc65c414548c9eb63018292b9133bd966259
acdelarosa <andlr31@hotmail.com>
2024-05-09 20:50:26 -0500

    eliminé cache

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express")
-const connectDb = require("./database/db")
-const userRoutes = require("./routes/usersRoutes")
-const authRoutes = require("./routes/authRoutes")
+const express = require("express");
+const connectDb = require("./database/db");
+const userRoutes = require("./routes/usersRoutes");
+const authRoutes = require("./routes/authRoutes");
 const sessionRoutes = require("./routes/sessionRoutes")
 const app = express()
 const PORT = 3010
@@ -15,6 +15,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 
 app.use("/api/session", sessionRoutes)
+
 
 
 app.listen(PORT, () => {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/authControllers");
 const verifyToken= require("../middleware/verifytoken")
 
+
 //login
 
 router.post("/login", authController.login);
